80f6f42fc66ce6c8b28a472c83b94367e60c1eb8
mpanodavid397 <mpanodavid397@gmail.com>
2026-05-21 16:21:09 +0200

    hamburger menu

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.nan');
+const navMenu = document.querySelector('.navList');
+const app = document.querySelector(".socials")
+
+menu.addEventListener('click', function () {
+    navMenu.classList.toggle('hidden');
+});
